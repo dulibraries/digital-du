1627db727d87402aa9dcda386039f91b0a7f2c45
Jeremy Nelson <jermnelson@gmail.com>
2016-01-22 11:48:41 -0700

    Updated mods repair

--- repair/mods.py
+++ repair/mods.py
@@ -15,6 +15,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(BASE_DIR)
 from instance import conf as CONF
 
+class RepairMODSError(Exception):
+
+    def __init__(self, pid, message):
+        self.pid = pid
+        self.message = message
+
+    def __str__(self):
+        return "Error with {}'s MODS\n{}".format(self.pid, self.message)
+
 def update_mods(pid, field_xpath, old_value, new_value):
     """Function takes pid, field, and old_value, replaces with
 	a new_value.
@@ -25,11 +34,12 @@ def update_mods(pid, field_xpath, old_value, new_value):
 		old_value -- 
 		new_value --
     """
-    mods_url = "{}{}/datastreams/MODS/content".format(
+    mods_base_url = "{}{}/datastreams/MODS/".format(
         CONF.REST_URL,
         pid)
+    get_mods_url = "{}content".format(mods_base_url)
     mods_result = requests.get(
-        mods_url,
+        get_mods_url,
         auth=CONF.FEDORA_AUTH)
     if mods_result.status_code > 399:
         err_title = """"Failed to replace {} with {} for PID {}, 
@@ -44,4 +54,15 @@ error={} url={}""".format(
     for element in old_value_elements:
         if element.text == old_value:
             element.text = new_value
-    print(etree.tostring(mods_xml).decode())
+    put_result = requests.put(
+        mods_base_url,
+        files={"file": ('MODS', etree.tostring(mods_xml))},
+        auth=CONF.FEDORA_AUTH)
+    if put_result.status_code < 300:
+        return True
+    else:
+        raise RepairMODSError(
+            pid, 
+            "Failed to update MODS with PUT\nStatus code {}\n{}".format(
+                put_result.status_code,
+                put_result.text))
