4ca129c2c1bd02bae9ab2f311fc690eaafd0c12a
Jeremy Nelson <jermnelson@gmail.com>
2016-03-08 09:47:06 -0700

    added CONTENTDM harvester

---
+++ repair/contentdm_harvester.py
@@ -0,0 +1,24 @@
+__author__ "Jeremy Nelson, Sarah Bogard"
+
+import csv
+import requests
+
+GET_FILE_URL = "http://cdm16304.contentdm.oclc.org/utils/getfile/collection/"
+
+def geology_thin_sections():
+    pass
+
+def gypsy_ames():
+    pass
+
+def ideas_merged():
+    pass
+
+def harvest():
+    gypsy_ames()
+    ideas_merged()
+    geology_thin_sections()
+    
+
+def __name__ == "__main__":
+    harvest()
